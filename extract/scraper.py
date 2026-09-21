148ba17412ebebff08849ec04d672d015cd59533
idni-tav <idni_tav@protonmail.com>
2026-09-21 16:02:40 +0100

    preserve collection category during extraction

--- extract/scraper.py
+++ extract/scraper.py
@@ -14,24 +14,27 @@ def run_extraction(garment_per_category):
     category_links=normalize_links(base_url,category_locators)                              
 
     product_links=[]
-    for link in category_links:
+    for category_link in category_links:
 
-        soup=get_soup(link)
+        soup=get_soup(category_link)
         raw_urls=extract_product_url(soup,product_locator)[:garment_per_category]
         normalized_urls=normalize_links(base_url,raw_urls)
+        category=category_link.rstrip("/").split("/")[-1]
+
 
         for url in normalized_urls:
-            product_links.append(url)
+            product_links.append((url,category))
     
     
     product_links=list(set(product_links))
 
     products=[]
-    for link in product_links:
+    for link, category in product_links:
         product_soup=get_soup(link)
         json_data=extract_json_block(product_soup)
         materials=extract_materials(product_soup,materials_locator)
         product=build_product_dict(json_data,materials)
+        product["category"]=category
         products.append(product)
 
 
